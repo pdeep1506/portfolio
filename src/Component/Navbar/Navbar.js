4c6fd6fdae990cf26d191dbd5a071a0c99cc240b
pdeep1506 <pdeep1506@gmail.com>
2023-09-23 18:52:54 -0400

    navbar completed

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from 'react-icons/ai'
 const Navbar = () => {
+    const [showBtn, setshowBtn] = useState(false);
   return (
     <div className='navbar'>
         <Link to='/'>
             <h1>Deep Patel</h1>
         </Link>
 
-        <ul className='nav-menu'>
+        <ul className={showBtn?"nav-menu":"nav-menu-active"}>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About Us</Link></li>
           <li><Link to='/project'>Project</Link></li>
           <li><Link to='/contact'>Contact</Link></li>
         </ul>
+
+        <div className='hamburger'>
+        {
+            showBtn ?
+            <FaBars size={20} style={{color:"#fff"}} onClick={()=>setshowBtn(!showBtn)}/>:<AiOutlineClose size={20}style={{color:"#fff"}} onClick={()=>setshowBtn(!showBtn)} ></AiOutlineClose>
+        }
+        </div>
     </div>
   )
 }
