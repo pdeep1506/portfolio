8a450146b872e5b5fccb3d1d0a1ff8ee48c1722e
pdeep1506 <pdeep1506@gmail.com>
2023-09-28 19:22:40 -0400

    updated project imgs

--- src/Component/Work/WorkCardData.js
+++ src/Component/Work/WorkCardData.js
@@ -1,10 +1,13 @@
-import pro1 from "../../assets/project1.png";
-import pro2 from "../../assets/project2.png";
-import pro3 from "../../assets/project3.png";
+import inshort from '../../assets/inshort.png'
+import quote from '../../assets/quote.png'
+import libraryManagement from '../../assets/library_management.jpg'
+import restaurent from '../../assets/restaurant.png'
+import cryptohunter from '../../assets/cryptohunter.png'
+import expense from '../../assets/expense.png';
 
 const ProjectCardData = [
   {
-    imgsrc: pro1,
+    imgsrc: libraryManagement,
     title: "Library management",
     text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
    
@@ -12,7 +15,7 @@ const ProjectCardData = [
     github:"https://github.com/pdeep1506/library_management.git"
   },
   {
-    imgsrc: pro2,
+    imgsrc: restaurent,
     title: "Restaurant",
     text: "Podcast Thriving Facebook community and top-ranked, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
    
@@ -20,31 +23,31 @@ const ProjectCardData = [
     github:"https://github.com/pdeep1506/restaurant.git"
   },
   {
-    imgsrc: pro3,
+    imgsrc: inshort,
     title: "Inshore Clone",
     text: "Top-ranked Thriving Facebook community and podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-    isLive:false,
+    isLive:"https://deepinshortclone.netlify.app/",
     github:"https://github.com/pdeep1506/inshort_clone.git"
   },
+  // {
+  //   imgsrc: expense,
+  //   title: "Expense Tracker",
+  //   text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
+  //   view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
+  //   isLive:false,
+  //   github:"https://github.com/pdeep1506/expenseTracker.git"
+  // },
   {
-    imgsrc: pro1,
-    title: "Expense Tracker",
-    text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-    isLive:false,
-    github:"https://github.com/pdeep1506/expenseTracker.git"
-  },
-  {
-    imgsrc: pro2,
+    imgsrc: quote,
     title: "Quote Generator",
     text: "Podcast Thriving Facebook community and top-ranked, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-    isLive:"https://www.youtube.com/watch?v=P8YuWEkTeuE",
+    isLive:"https://deeprandomquote.netlify.app/",
     github:"https://github.com/pdeep1506/Quote-Generator-reactjs.git"
   },
   {
-    imgsrc: pro3,
+    imgsrc: cryptohunter,
     title: "Crypto-hunter",
     text: "Top-ranked Thriving Facebook community and podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
