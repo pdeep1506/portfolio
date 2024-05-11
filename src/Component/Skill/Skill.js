c27b43ba872e031dac8060398d86a83d41f610a3
pdeep1506 <pdeep1506@gmail.com>
2024-05-11 12:13:11 -0400

    working on skill page

--- src/Component/Skill/Skill.js
+++ src/Component/Skill/Skill.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Skill.css'
 import HeroImg2 from '../../Component/HeroImg2/HeroImg2.js'
-import cryptohunter from '../../assets/cryptohunter.png'
+import html from '../../assets/skills/html.svg'
 
 
 const Skill = () => {
@@ -9,16 +9,169 @@ const Skill = () => {
     <div>
         <HeroImg2 heading="Skills." text="Lets look at my skills" />
 
-<div className='skill--card'>
+<div className='skill-container'>
 
-        <div className='skill--item '>
-        {/* <img src={nodejs} className='img' alt="image" /> */}
+        //!Frontend Development
+          <div className='boxes box-1'>
+          <h2>Frontend Development</h2>
+          <div className='box1'>
+                <div className='box'>
+                    <img src={html}></img>
+                    HTML
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    CSS3
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    JavaScript
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    React Js
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    Redux
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    Tailwind
+                </div>
+          </div>
+          </div>
 
-            <p>html</p>
-        </div>
+        //! Backend Development
+
+          <div className='boxes box-2'>
+          <h2>Backend Development</h2>
+          <div className='box1'>
+                <div className='box'>
+                    <img src={html}></img>
+                    Node Js
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    Express Js
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    MongoDB
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    MySql
+                </div>
+
+                
+               
+          </div>
+          </div>
+
+
+          //! Programming Language
+          <div className='boxes box-3'>
+          <h2>Programming Language</h2>
+          <div className='box1'>
+                <div className='box'>
+                    <img src={html}></img>
+                    C
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    Java
+                </div>
+                
+                
+          </div>
+          </div>
+
+          //! Version Control & Development
+          <div className='boxes box-4'>
+          <h2>Version Control & Development</h2>
+          <div className='box1'>
+                <div className='box'>
+                <img src={html}></img>
+                Git
+                </div>
+                <div className='box'>
+                    <img src={html}></img>
+                    GitHub
+                </div>
+               
+          </div>
+          </div>
+
+
+          //! Cloud Computing
+          <div className='boxes box-5'>
+          <h2>Cloud Computing</h2>
+          <div className='box1'>
+                <div className='box'>
+                    <img src={html}></img>
+                    AWS
+                </div>
+                
+                
+          </div>
+          </div>
+
+        //! Agile Methodology
+          <div className='boxes box-6'>
+          <h2>Agile Methodology</h2>
+          <div className='box1'>
+                <div className='box'>
+                Scrum
+                </div>
+               
+          </div>
+          </div>
+
+        //! Tools
+          <div className='boxes box-7'>
+          <h2>Tools</h2>
+          <div className='box1'>
+                <div className='box'>
+                   
+                    VS Code
+
+                </div>
+                <div className='box'>
+               
+                    Postman
+                </div>
+
+          </div>
+          </div>
+
+        //! concept
+          <div className='boxes box-8'>
+          <h2>Concepts</h2>
+          <div className='box1'>
+                <div className='box'>
+                   
+                Object Oriented Programming
+
+                </div>
+                <div className='box'>
+                Data structure & Algorithm
+                </div>
+                <div className='box'>
+                Data base management system
+                </div>
+                <div className='box'>
+                Software Engineering
+                </div>
+                
+          </div>
+          </div>
         
-</div>
-    </div>
+          </div>
+       </div>
+        
+
+    
   )
 }
 
